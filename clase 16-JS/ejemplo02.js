98367ad2ce950afb2d4564ff3b3cd5d55f808628
Ana Lucia Arias <ariasdelosheros@gmail.com>
2022-10-24 17:30:28 -0300

    JS

---
+++ clase 16-JS/ejemplo02.js
@@ -0,0 +1,6 @@
+const link = document.getElementById('link');
+
+link.addEventListener('click', function (e){
+    e.preventDefault();//frena que funcione el link//
+    alert('Quisiste ir a: ' + e.currentTarget.href)
+})
