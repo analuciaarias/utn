7507c0473998fde3721bf3dfc196fef1c421c82d
Ana Lucia Arias <ariasdelosheros@gmail.com>
2022-10-26 11:07:45 -0300

    JS- con CSS

---
+++ clase 16-JS/ejemplo06.js
@@ -0,0 +1,8 @@
+const alternar = document.getElementsByClassName('alternar')[0];
+alternar.addEventListener('click', function(e){
+    e.currentTarget.classList.toggle('activo');
+});
+//classList representa una lista de clases asignadas al elemento.
+//toggle cambiar una clase
+//add agrega una clase
+// remove remueve una o mas clases
